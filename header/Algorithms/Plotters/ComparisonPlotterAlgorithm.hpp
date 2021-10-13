2f903814ff1e1294053db4e91495b2921eaf25e9
Carlo Varni <carlovarni@MacBook-Pro-di-Carlo-2.local>
2021-10-13 15:03:29 -0700

    Changes

--- header/Algorithms/Plotters/ComparisonPlotterAlgorithm.hpp
+++ header/Algorithms/Plotters/ComparisonPlotterAlgorithm.hpp
@@ -77,7 +77,9 @@ namespace Algorithm {
 
     std::vector<const object_collection_t*> object_collections;
     std::vector<const std::unordered_map<std::string, std::size_t>*> object_maps;
-
+    object_collections.reserve(m_cfg.originalInputCollection.size());
+    object_maps.reserve(m_cfg.originalInputCollection.size());
+    
     for (const std::string& input : m_cfg.originalInputCollection ) {
       object_collections.push_back( context.get<object_collection_t>( m_cfg.prefix + "_" + input) );
       object_maps.push_back( context.get<std::unordered_map<std::string, std::size_t>>("index_" + m_cfg.prefix + "_" + input) );
@@ -85,18 +87,17 @@ namespace Algorithm {
 
     for (const std::string& var_name : m_cfg.variableNames) {
       object_collection_t object_to_be_drawn;
+      object_to_be_drawn.reserve(object_maps.size());
       
       for (auto ientry(0); ientry < object_maps.size(); ientry++) {
 	const auto& map = object_maps.at(ientry);
 	if (map->find( m_cfg.prefix + "_" + var_name) == map->end())
-	  {
-	    for (auto el : *map) std::cout<<el.first<<std::endl; 
 	  throw std::runtime_error("Variable not available in collection: " + var_name);
-	  }
+	
 	std::size_t index = map->at( m_cfg.prefix + "_" + var_name);
 	object_to_be_drawn.push_back( object_collections.at(ientry)->at(index) );
       }
-
+      
       TCanvas canvas("canvas", "canvas");
       DrawCollection(canvas,
 		     object_to_be_drawn);
@@ -104,7 +105,7 @@ namespace Algorithm {
       
       std::string save_name = m_cfg.outputFolder + "/comparison_" + m_cfg.prefix + "_" + var_name + ".pdf";
       canvas.SaveAs( save_name.c_str() );
-      MSG_INFO( "Created histogram: " + save_name );
+      MSG_INFO( "Created plot: " + save_name );
     }
   }
 
