8814222744746c06436e9df8c0e6024ba85c7b4d
Psicognomo <psicognomo.lognomopsicopatico@gmail.com>
2021-10-14 01:56:07 -0700

    Changes

---
+++ .CI/check_format.sh
@@ -0,0 +1,37 @@
+
+# abort on error
+set -e
+
+
+if [ $# -ne 1 ]; then
+    echo "<DIR> path not specified"
+    exit 1
+fi
+
+_binary=${CLANG_FORMAT_BINARY:-clang-format}
+
+$_binary --version
+
+cd $1
+if ! [ -z $CI ] || ! [ -z $GITHUB_ACTIONS ]; then
+  mkdir changed
+  for f in $(git diff --name-only); do
+    cp --parents $f changed
+  done
+fi
+    
+echo "clang-format done"
+    
+set +e
+git diff --exit-code --stat
+result=$?
+
+if [ "$result" -eq "128" ]; then
+    echo "Format was successfully applied"
+    echo "Could not create summary of affected files"
+    echo "Are you in a submodule?"
+
+fi
+
+exit $result
+
